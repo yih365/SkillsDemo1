9be080efed4815a87debb10ab15aa2d40cdda77e
yih365 <68409635+gigi365@users.noreply.github.com>
2022-02-03 20:29:59 -0800

    Update SkillsDemoTest.java

--- SkillsDemoTest.java
+++ SkillsDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillsDemoTest {
 	@Test
 	public void testMultiplication() {
-		assertEquals(5, SkillsDemo.multiply(2, 3));
+		assertEquals(6, SkillsDemo.multiply(2, 3));
 	}
 }
